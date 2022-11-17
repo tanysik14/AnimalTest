abddb9b09c178fe17557dc2284e9cde26b001dec
HelloWorld <tanysik14>
2022-11-17 12:32:46 +0300

    Initial Commit

---
+++ AnimalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalTest
+//
+//  Created by Tatsiana Pauliukova on 17.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
